8d8ea09877114943fb36c71f68a81478bd804154
Francisco Milani <65914041+FranciscoMilani@users.noreply.github.com>
2023-09-05 23:34:45 -0300

    first commit

---
+++ src/main/java/br/ucs/classleague/ClassLeague.java
@@ -0,0 +1,12 @@
+package br.ucs.classleague;
+
+/**
+ *
+ * @author Francisco
+ */
+public class ClassLeague {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
