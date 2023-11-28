50a6819c3e828ce050f22b2500c3a673932dae19
Francisco Fornasier Milani <65914041+FranciscoMilani@users.noreply.github.com>
2023-11-28 14:38:04 -0300

    Correção no cache git

--- src/main/java/br/ucs/classleague/domain/sports/Basketball.java
+++ src/main/java/br/ucs/classleague/domain/sports/Basketball.java
@@ -5,8 +5,8 @@ import br.ucs.classleague.domain.Sport;
 public class Basketball extends Sport {
     
     private static final SportsEnum sport = SportsEnum.BASKETBALL;
-    private static final Integer matchDurationMinutes = 45;
-    private static final Integer halfAmount = 2;
+    private static final Integer matchDurationMinutes = 10;
+    private static final Integer halfAmount = 4;
     private static final Boolean pointsPerPlayer = true;
     
     public Basketball() {
